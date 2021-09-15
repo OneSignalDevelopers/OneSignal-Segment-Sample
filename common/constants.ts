1bf4cda7ed5e45d2c9aea9bd082c482e06ea5327
William Shepherd <iAmWillShepherd@users.noreply.github.com>
2021-09-15 15:59:26 -0500

    Load secrets from env

---
+++ common/constants.ts
@@ -0,0 +1,3 @@
+export const SegmentWriteKey = process.env.SEGMENT_WRITE_KEY!;
+export const OneSignalAppId = process.env.NEXT_PUBLIC_ONESIGNAL_APP_ID!;
+export const OneSignalSecretKey = process.env.NEXT_PUBLIC_ONESIGNAL_API_KEY!;
