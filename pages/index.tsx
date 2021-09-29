9a57e706d58f7ebb3ce475c7acb64807efe904f5
William Shepherd <williamrshepherd@gmail.com>
2021-09-29 10:34:01 -0500

    Try to catch the errors

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,47 +1,46 @@
 import { Field, Form, Formik } from "formik";
 import type { NextPage } from "next";
-import { useRouter } from "next/router";
 import Head from "next/head";
+import { useRouter } from "next/router";
 import React from "react";
 import { UserSignupInfo } from "../common/types";
-import { createSecureHash } from "../common/utils/secure-hash";
-import useOneSignal from "../components/hooks/useOneSignal";
 import OneSignal from "../components/OneSignal";
 import styles from "../styles/Home.module.css";
 
 const Home: NextPage = () => {
   const router = useRouter();
-
-  useOneSignal();
-
   const signup = async (data: UserSignupInfo) => {
-    const response = await fetch("api/signup", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(data),
-    });
-    const json = await response.json();
-    const { id } = json;
-    const hash = createSecureHash(id);
-    await window.OneSignal.setExternalUserId(id, hash);
-    router.push(`/onboard/${id}`);
+    try {
+      const response = await fetch("api/signup", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(data),
+      });
+      const json = await response.json();
+      const { userId } = json;
+
+      await window.OneSignal.setEmail(data.email);
+      await window.OneSignal.setExternalUserId(userId);
+      router.push(`/onboard/${userId}`);
+    } catch (err) {
+      console.error(err);
+    }
   };
 
   return (
     <>
+      <Head>
+        <title>Wubba Lubba Dub-Dub!</title>
+        <meta
+          name="description"
+          content="The same stuff, just event based bruh"
+        />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
       <OneSignal />
       <div className={styles.container}>
-        <Head>
-          <title>Wubba Lubba Dub-Dub!</title>
-          <meta
-            name="description"
-            content="The same stuff, just event based bruh"
-          />
-          <link rel="icon" href="/favicon.ico" />
-        </Head>
-
         <main className={styles.main}>
           <h1 className={styles.title}>Welcome to Wubba Lubba Dub-Dub!</h1>
 
