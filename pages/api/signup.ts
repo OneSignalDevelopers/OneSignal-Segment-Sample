a479b6f4e789a8b3df2ef575e7dfe6c4326101fe
William Shepherd <williamrshepherd@gmail.com>
2021-09-29 10:33:17 -0500

    Update to work with latest types

--- pages/api/signup.ts
+++ pages/api/signup.ts
@@ -1,30 +1,38 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from "next";
 import Segment from "analytics-node";
+import type { NextApiRequest, NextApiResponse } from "next";
 import { SegmentWriteKey } from "../../common/constants";
-import { User, UserSignupInfo } from "../../common/types";
+import { UserSignupInfo } from "../../common/types";
+import {
+  exists as userExists,
+  insert as insertUser,
+} from "../../common/utils/fake-user-store";
+
+type SignupResponse = { userId: number } | { error: string };
 
 const segment = new Segment(SegmentWriteKey);
 
 export default function handler(
   req: NextApiRequest,
-  res: NextApiResponse<{ id: string } | { error: string }>
+  res: NextApiResponse<SignupResponse>
 ) {
   const { email } = req.body as UserSignupInfo;
-  if (!email) return res.status(422).json({ error: "name is required" });
-  // logic that creates a new user...
-  const id = "123";
+  if (!email) return res.status(422).json({ error: "Email is required." });
+  if (userExists(email))
+    return res
+      .status(422)
+      .json({ error: `User with email ${email} already exists.` });
+
+  const userId = insertUser(email);
   segment.track(
     {
-      userId: id,
+      userId: userId,
       event: "Signup",
       properties: {
         email,
-        test: "test",
       },
     },
     err => err && console.error(err)
   );
 
-  return res.status(200).json({ id });
+  return res.status(200).json({ userId });
 }
